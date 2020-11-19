d6fbb3f2edab1db8f41f366f713bde76647dc4fe
Jarkko Mämmelä <jarkko.mammela@gmail.com>
2020-11-19 13:58:16 +0200

    fix other utf-8 problems

--- src/main/java/Scenes/AddBookScene.java
+++ src/main/java/Scenes/AddBookScene.java
@@ -10,6 +10,7 @@ import javafx.scene.layout.VBox;
 import Database.db;
 
 public class AddBookScene {
+
     TextField kirjoittaja;
     TextField otsikko;
     TextField tyyppi;
@@ -17,7 +18,7 @@ public class AddBookScene {
     TextField tagit;
     TextField releatedCourses;
     Button submitButton;
-    
+
     public AddBookScene() {
         this.kirjoittaja = new TextField();
         this.otsikko = new TextField();
@@ -25,56 +26,56 @@ public class AddBookScene {
         this.ISBN = new TextField();
         this.tagit = new TextField();
         this.releatedCourses = new TextField();
-        this.submitButton = new Button("Lisää uusi kirja");
+        this.submitButton = new Button("LisÃ¤Ã¤ uusi kirja");
     }
-    
+
     public Scene createScene() {
         BorderPane addBookPane = new BorderPane();
         VBox addBookVBox = new VBox();
-        addBookVBox.setPadding(new Insets(70,20,20,20));
-        
+        addBookVBox.setPadding(new Insets(70, 20, 20, 20));
+
         kirjoittaja.setPromptText("Kirjoittaja");
         otsikko.setPromptText("Otsikko");
         tyyppi.setPromptText("Tyyppi");
         ISBN.setPromptText("ISBN");
         tagit.setPromptText("Tagit");
         releatedCourses.setPromptText("Releated Courses");
-        
+
         submitButton.setOnAction(e -> {
             lisaaKirja();
         });
-        
+
         addBookVBox.getChildren().addAll(kirjoittaja, otsikko, tyyppi, ISBN, tagit, releatedCourses, submitButton);
-        
+
         Scene addBookScene = new Scene(addBookVBox, 600, 400);
-        
+
         return addBookScene;
     }
-    
+
     public void lisaaKirja() {
-        
+
     }
-    
+
     public TextField getKirjoittajaText() {
         return this.kirjoittaja;
     }
-    
+
     public TextField getOtsikkoText() {
         return this.otsikko;
     }
-    
+
     public TextField getTyyppiText() {
         return this.tyyppi;
     }
-    
+
     public TextField getISBNText() {
         return this.ISBN;
     }
-    
+
     public TextField getreleatedCoursesText() {
         return this.releatedCourses;
     }
-    
+
     public TextField getTagitText() {
         return this.tagit;
     }
--- src/main/java/Scenes/AddURLScene.java
+++ src/main/java/Scenes/AddURLScene.java
@@ -10,64 +10,65 @@ import javafx.scene.layout.VBox;
 import Database.db;
 
 public class AddURLScene {
+
     TextField otsikko;
     TextField tyyppi;
     TextField kommentti;
     TextField URL;
     TextField releatedCourses;
     Button submitButton;
-    
+
     public AddURLScene() {
         this.otsikko = new TextField();
         this.tyyppi = new TextField();
         this.URL = new TextField();
         this.kommentti = new TextField();
         this.releatedCourses = new TextField();
-        this.submitButton = new Button("Lisää uusi URL");
+        this.submitButton = new Button("LisÃ¤Ã¤ uusi URL");
     }
-    
+
     public Scene createScene() {
         BorderPane addBookPane = new BorderPane();
         VBox addBookVBox = new VBox();
-        addBookVBox.setPadding(new Insets(70,20,20,20));
-        
+        addBookVBox.setPadding(new Insets(70, 20, 20, 20));
+
         otsikko.setPromptText("Otsikko");
         URL.setPromptText("URL");
         releatedCourses.setPromptText("Releated Courses");
         tyyppi.setPromptText("Tyyppi");
         kommentti.setPromptText("Kommentti");
-        
+
         submitButton.setOnAction(e -> {
             lisaaURL();
         });
-        
-        addBookVBox.getChildren().addAll(otsikko, URL, tyyppi, releatedCourses,kommentti, submitButton);
-        
+
+        addBookVBox.getChildren().addAll(otsikko, URL, tyyppi, releatedCourses, kommentti, submitButton);
+
         Scene addBookScene = new Scene(addBookVBox, 600, 400);
-        
+
         return addBookScene;
     }
-    
+
     public void lisaaURL() {
         System.out.println("testi");
     }
-    
+
     public TextField getKommenttiText() {
         return this.kommentti;
     }
-    
+
     public TextField getOtsikkoText() {
         return this.otsikko;
     }
-    
+
     public TextField getTyyppiText() {
         return this.tyyppi;
     }
-    
+
     public TextField getURLText() {
         return this.URL;
     }
-    
+
     public TextField getreleatedCoursesText() {
         return this.releatedCourses;
     }
